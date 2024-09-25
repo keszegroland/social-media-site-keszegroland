9072131b055788007bdea2e69326f65c4ea8d3d0
rolandkeszeg <78346746+keszegroland@users.noreply.github.com>
2024-09-25 16:59:05 +0200

    created and designed signup page

---
+++ Frontend/src/Pages/SignUp.tsx
@@ -0,0 +1,10 @@
+import SignUpForm from "../Components/SignupForm/SignUpForm";
+
+function SignUpPage() {
+
+  return <div className="flex items-center justify-center min-h-screen">
+    <SignUpForm />
+  </div>
+}
+
+export default SignUpPage;
