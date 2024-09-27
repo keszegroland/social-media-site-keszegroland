ec44122493342cfc8759a5304c2d628d3f494779
rolandkeszeg <78346746+keszegroland@users.noreply.github.com>
2024-09-27 10:56:56 +0200

    redesigned the signup page to be responsive

--- Frontend/src/Pages/SignUp.tsx
+++ Frontend/src/Pages/SignUp.tsx
@@ -2,7 +2,7 @@ import SignUpForm from "../Components/FormComponents/SignUpForm";
 
 function SignUp() {
 
-  return <div className="flex items-center justify-center min-h-screen max-w-[386px] max-h-[552px]">
+  return <div className="flex items-center justify-center min-h-screen">
     <SignUpForm />
   </div>
 }
