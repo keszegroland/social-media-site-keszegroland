5441c449ac64e004a7f186ff2c624349deac22b8
rolandkeszeg <78346746+keszegroland@users.noreply.github.com>
2024-09-29 18:30:37 +0200

    created header navbar

--- Frontend/src/Pages/SignIn.tsx
+++ Frontend/src/Pages/SignIn.tsx
@@ -1,10 +1,12 @@
 import Background from "../Components/Background/Background";
+import Header from "../Components/FormComponents/Header";
 import SignInForm from "../Components/FormComponents/SignInForm";
 
 function SignIn() {
 
   return (
     <div className="relative w-screen h-screen bg-base-300 sm:bg-base-100">
+      <Header />
       <SignInForm />
       <Background />
     </div>
