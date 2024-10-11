a745e3a0db66f7f258bf82ecb5f869c08dfae1d7
rolandkeszeg <78346746+keszegroland@users.noreply.github.com>
2024-10-11 20:21:07 +0200

    added like information text and comment button and refactored the FooterPost component and added stroke to the liked.svg

--- Frontend/src/Components/Home/Post/FooterPost.tsx
+++ Frontend/src/Components/Home/Post/FooterPost.tsx
@@ -1,9 +1,20 @@
 import { useEffect, useState } from "react";
 import getToken, { JWTTokenType } from "../../../Utils/getToken";
+import SaveButton from "./SaveButton";
+
+interface Props {
+  postPublicId: string;
+};
+
+interface LikeData {
+  isPostLiked: boolean;
+  usernameOfTheFirstLiker: string;
+  numberOfLikes: number;
+};
 
 type MethodType = "GET" | "POST" | "DELETE";
 
-async function handleLikeRequest(method: MethodType, path: string, token: JWTTokenType) {
+async function handleLikeRequest(method: MethodType, path: string, token: JWTTokenType): Promise<LikeData | string> {
   const response = await fetch(`/api/likes/${path}`, {
     method: method,
     headers: {
@@ -12,60 +23,80 @@ async function handleLikeRequest(method: MethodType, path: string, token: JWTTok
     }
   });
 
-  return await response.json();
-}
+  const data = await response.json();
 
-interface Props {
-  postPublicId: string;
+  if (method === "GET") {
+    return data as LikeData;
+  }
+
+  return data as string;
 }
 
 function FooterPost({ postPublicId }: Props) {
-  const [likeState, setLikeState] = useState<boolean>(false);
+  const [isLiked, setIsLiked] = useState<boolean>(false);
+  const [numberOfLikes, setNumberOfLikes] = useState<number>(0);
+  const [usernameOfTheFirstLiker, setUsernameOfTheFirstLiker] = useState<string>("");
   const token = getToken();
 
-  async function handleLikeAction(action: "like" | "unlike") {
-    const isLiking = action === "like";
-    setLikeState(isLiking);
+  useEffect(() => {
+    async function getLikeDataForPost() {
+      const response = await handleLikeRequest("GET", `data/${postPublicId}`, token);
+      if (typeof response === "object" && "isPostLiked" in response && "usernameOfTheFirstLiker" in response && "numberOfLikes" in response) {
+        const { isPostLiked, usernameOfTheFirstLiker, numberOfLikes } = response;
+        setIsLiked(isPostLiked);
+        setNumberOfLikes(numberOfLikes);
+        setUsernameOfTheFirstLiker(usernameOfTheFirstLiker);
+      }
+    }
+    getLikeDataForPost();
+  }, [postPublicId, token])
+
+  async function handleLikeAction() {
+    const updatedIsLiked = !isLiked;
+    const updatedNumberOfLikes = updatedIsLiked ? (numberOfLikes + 1) : (numberOfLikes - 1);
+    setIsLiked(updatedIsLiked);
+    setNumberOfLikes(updatedNumberOfLikes);
 
     try {
-      const method = isLiking ? "POST" : "DELETE";
-      await handleLikeRequest(method, `${action}/${postPublicId}`, token);
+      const method = updatedIsLiked ? "POST" : "DELETE";
+      await handleLikeRequest(method, `${updatedIsLiked ? "like" : "unlike"}/${postPublicId}`, token);
     } catch (error) {
       console.error(error);
-      setLikeState(!isLiking);
+      setIsLiked(isLiked);
+      setNumberOfLikes(numberOfLikes);
     }
   }
 
-  useEffect(() => {
-    async function getLikesForPosts() {
-      const likes: string[] = await handleLikeRequest("GET", "", token);
-      setLikeState(likes.includes(postPublicId));
+  function handleLikeText(): JSX.Element {
+    if (numberOfLikes === 0) {
+      return <></>;
     }
-    getLikesForPosts();
-  }, [token, postPublicId])
 
-  function handleLikeMechanism() {
-    if (likeState) {
-      handleLikeAction("unlike");
-    } else {
-      handleLikeAction("like");
-    }
+    return numberOfLikes === 1 ? (
+      <><strong>{numberOfLikes}</strong> like.</>
+    ) : (
+      <><strong>{usernameOfTheFirstLiker}</strong> and <strong>{numberOfLikes - 1}</strong> other(s) like this.</>
+    )
   }
+
 
   return (
-    <div className="flex w-full justify-between items-center">
-      <label className="swap swap-rotate cursor-pointer">
-        <input type="checkbox" checked={likeState} onChange={handleLikeMechanism} />
-        <img className="swap-on" src="/liked.svg" alt="liked"></img>
-        <img className="swap-off" src="/like.svg" alt="like"></img>
-      </label>
-      <label className="swap swap-rotate cursor-pointer">
-        <input type="checkbox" />
-        <img className="swap-on" src="/saved.svg" alt="Saved"></img>
-        <img className="swap-off" src="/save.svg" alt="Save"></img>
-      </label>
+    <div className="flex flex-col w-full">
+      <div className="flex w-full justify-between">
+        <div className="flex gap-2">
+          <label className="swap swap-rotate cursor-pointer">
+            <input type="checkbox" checked={isLiked} onChange={handleLikeAction} />
+            <img className="swap-on" src="/liked.svg" alt="liked"></img>
+            <img className="swap-off" src="/like.svg" alt="like"></img>
+          </label>
+          <img className="cursor-pointer" src="/comment.svg" alt="comment"></img>
+        </div>
+        <SaveButton />
+      </div>
+      <p className="text-xs md:text-sm text-left w-full pl-1">{handleLikeText()}</p>
     </div>
-  )
+  );
+
 }
 
 export default FooterPost;
