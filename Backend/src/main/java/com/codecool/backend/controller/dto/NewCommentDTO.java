8d5db418e419d5b6f4a7fbfe535beab0d48cc5ae
rolandkeszeg <78346746+keszegroland@users.noreply.github.com>
2024-12-06 12:24:15 +0100

    implemented comment functionality to retrieve all comments and create new ones

--- Backend/src/main/java/com/codecool/backend/controller/dto/NewCommentDTO.java
+++ Backend/src/main/java/com/codecool/backend/controller/dto/NewCommentDTO.java
@@ -1,6 +1,4 @@
 package com.codecool.backend.controller.dto;
 
-import java.util.UUID;
-
-public record NewCommentDTO(String comment, String username, UUID postPublicId) {
+public record NewCommentDTO(String comment) {
 }
